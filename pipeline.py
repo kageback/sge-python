a31f007a702b95feb5702e4456bddee0fc6187e7
kageback <kageback@chalmers.se>
2018-05-20 22:21:52 +0200

    made hyper param search experiment subclass to pipeline

--- pipeline.py
+++ pipeline.py
@@ -5,12 +5,14 @@ from functools import reduce
 
 from gridengine.task import Task
 from gridengine.result_wrapper import ResultWrapper
-
+from gridengine.queue import Local
 
 class Pipeline:
-    def __init__(self, queue, pipelines_path='pipelines', pipeline_id_prefix='pl', hyperparams=None):
+    def __init__(self, queue=None, pipelines_path='pipelines', pipeline_id_prefix='pl'):
+        if queue is None:
+            queue = Local()
+
         self.queue = queue
-        self.hyperparams = hyperparams
 
         pipelines_path = os.path.dirname(pipelines_path + '/') + '/'
 
@@ -77,3 +79,53 @@ class Pipeline:
             return pickle.load(f)
 
 
+##### HyperParamSearchExperiment ######
+
+from copy import deepcopy
+import itertools
+from collections import OrderedDict
+
+import numpy as np
+class HyperParamSearchExperiment(Pipeline):
+
+    def __init__(self, ranges, exp_name='exp', queue=None, pipelines_path='pipelines'):
+        super().__init__(queue, pipelines_path, exp_name)
+
+        self.ranges = OrderedDict(ranges)
+        self.range_indices = OrderedDict([(r[0], range(len(r[1]))) for r in ranges])
+
+        self.axes = OrderedDict([(r[0], axis) for (r, axis) in zip(ranges, range(len(ranges)))])
+
+        self.shape = [len(r[1]) for r in ranges]
+        self.result_wrappers = OrderedDict()
+
+    def set_result(self, measure, index_coord, value):
+        def add(list_obj, coord, value):
+            if len(coord) > 1:
+                add(list_obj[coord[0]], coord[1:], value)
+            else:
+                list_obj[coord[0]] = value
+
+        if measure not in self.result_wrappers.keys():
+            self.result_wrappers[measure] = np.zeros(self.shape).tolist()
+
+        add(self.result_wrappers[measure], index_coord, value)
+
+
+    def to_numpy(self, output_name, result_index=0):
+        def unwrap_results(list_obj):
+
+            for i in range(len(list_obj)):
+                if type(list_obj[i]) is Task:
+                    list_obj[i] = list_obj[i].result(result_index).get()
+                else:
+                    unwrap_results(list_obj[i])
+
+        results = deepcopy(self.result_wrappers[output_name])
+        unwrap_results(results)
+
+        return np.array(results)
+
+    def __iter__(self):
+        #return itertools.product(*self.indexed_ranges.values())
+        return zip(itertools.product(*self.range_indices.values()), itertools.product(*self.ranges.values()))
