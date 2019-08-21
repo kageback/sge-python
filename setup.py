d7a0e5656f605abf24d3bad07867b78de10da3df
Mikael Kågebäck <mikael@kageback.se>
2019-08-21 13:45:51 +0200

    added setup.py to make the package installable with pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='expgraph',
+    version='0.2',
+    packages=['gridengine'],
+    url='https://github.com/kageback/sge-python',
+    license='MIT License',
+    author='Mikael Kågebäck',
+    author_email='mikael@kageback.se',
+    description='For running experiments. Simple or very complex. '
+)
